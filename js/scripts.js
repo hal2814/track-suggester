df9edf0a962d370bdc44a7a215fcbb0089a9125f
Nate McGregor <natecmcgregor@Lil-Nate.local>
2017-08-11 13:12:15 -0700

    add back-end logic and images

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,15 @@
+
+/*select (function(var1p,var2ct,val1,val2,output){
+  if((var1p===var2) && (var2ct===val2)){
+    $("#output").show();
+    $(".nameResult").append(name);
+    $(output).show();
+  }
+  return 0;
+});
+*/
 $(document).ready(function() {
-  selectPers (function(pers){
-    if((pers==="Creative"))
-    {
 
-    }
-    if((pers==="Tinkerer"))
-    {
-
-    }
-    if((pers==="Practical"))
-    {
-
-    }
-  selectCode (function(cT){
-    if((cT==="Mobile applications"))
-    {
-
-    }
-    if((cT==="Software"))
-    {
-
-    }
-    if((cT==="Web design"))
-    {
-
-    }
-  selectPlace (function(pl){
-    if((pl==="Seattle"))
-    {
-
-    }
-    if((pl==="Portland"))
-    {
-
-    }
-    if((pl==="Elsewhere"))
-    {
-
-    }
-  });
   $("form#track").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
@@ -48,7 +19,55 @@ $(document).ready(function() {
 
 
 
-    $(".nameResult").append(name);
-    $("#output").show();
+    select(personality,codeType,"Creative","Web design", "#cssDesign");
+
+    if((personality==="Creative") && (codeType==="Web design"))
+    {
+      $("#output").show();
+      $(".nameResult").append(name);
+      $("#cssDesign").show();
+    }
+    if((personality==="Tinkerer") && (codeType==="Mobile applications"))
+    {
+      $("#output").show();
+      $(".nameResult").append(name);
+      $("#javaAnd").show();
+    }
+    if((personality==="Practical") && (codeType==="Software"))
+    {
+      $("#output").show();
+      $(".nameResult").append(name);
+      $("#cSharp").show();
+    }
+    if((personality==="Creative") && (codeType==="Software"))
+    {
+      $("#output").show();
+      $(".nameResult").append(name);
+      $("#ruby").show();
+    }
+    if((personality==="Tinkerer") && (codeType==="Web design"))
+    {
+      $("#output").show();
+      $(".nameResult").append(name);
+      $("#php").show();
+    }
+    
+    if((place==="Seattle"))
+    {
+      $("#seattle").show();
+    }
+    if((place==="Portland"))
+    {
+      $("#portland").show();
+    }
+    if((place==="Elsewhere"))
+    {
+      if(personality==="Creative")
+      {
+        $("#portland").show();
+      }else{
+        $("#seattle").show();
+      }
+    }
   });
 });
