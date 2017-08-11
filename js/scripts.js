b0fce297ce5ac393fc81bd3f99efc573323186dd
Nate McGregor <natecmcgregor@Lil-Nate.local>
2017-08-11 10:20:57 -0700

    add basic framework for design.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#track").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
